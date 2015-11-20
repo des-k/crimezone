25fb74fd097edb8452f9bb5be0562229a215e666
Ade Sudiman <sudiman.ade@gmail.com>
2015-11-20 15:28:00 +0700

    crimezone/report_comment_ui.java: update reference to main toolbar

--- CrimeZone/CrimeZone/src/main/java/com/ppl/crimezone/report_comment_ui.java
+++ CrimeZone/CrimeZone/src/main/java/com/ppl/crimezone/report_comment_ui.java
@@ -18,7 +18,7 @@ public class report_comment_ui extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.report_comment_ui, menu);
+        getMenuInflater().inflate(R.menu.map_controller_actions, menu);
         return true;
     }
 
